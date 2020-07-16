cdb8d679cc61587d43c1a7222ed0eba424094da2
Marko Lisica <titelskibreg@gmail.com>
2020-07-16 11:44:28 +0200

    Tag test

--- debug.py
+++ debug.py
@@ -1,3 +1,13 @@
 import os
 
-print(os.environ['GITHUB_REF'].split('/')[2] + os.environ['GITHUB_RUN_ID'])
+ref = os.environ['GITHUB_REF'].split('/')[2]
+sha = os.environ['GITHUB_SHA'][:8]
+run_id = os.environ['GITHUB_RUN_ID']
+
+
+print(ref)
+print(sha)
+print(run_id)
+
+if 'tag-test' in ref:
+    print("Tag commit")
